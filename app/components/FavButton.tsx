af55b1d94997e5a9826d515edff1f83cda31c6e9
Destin Lee <dleeinc@gmail.com>
2023-11-13 17:39:27 -0600

    favs

--- app/components/FavButton.tsx
+++ app/components/FavButton.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { useRouter } from 'next/navigation'
 import { useEffect, useState } from 'react'
 
 interface FavButtonProps {
@@ -17,22 +18,22 @@ export default function FavButton({ chapter_id, fav_id }: FavButtonProps) {
     return false
   })
 
+  const router = useRouter()
+
   async function handleFav() {
+    setFaved(true)
     const supabase = createClientComponentClient()
     const { data, error } = await supabase
       .from('favorites')
       .insert({ chapter_id })
 
     if (error) {
-      alert(error)
       console.log(error)
       setFaved(false)
       return
     }
 
-    alert('faved')
-    console.log(data)
-    setFaved(true)
+    router.refresh()
   }
 
   async function handleDeleteFav() {
@@ -57,14 +58,14 @@ export default function FavButton({ chapter_id, fav_id }: FavButtonProps) {
           xmlns='http://www.w3.org/2000/svg'
           fill='none'
           viewBox='0 0 24 24'
-          stroke-width='1.5'
+          strokeWidth='1.5'
           stroke='currentColor'
           className='w-6 h-6 cursor-pointer'
           onClick={handleFav}
         >
           <path
-            stroke-linecap='round'
-            stroke-linejoin='round'
+            strokeLinecap='round'
+            strokeLinejoin='round'
             d='M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z'
           />
         </svg>
