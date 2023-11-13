cb745abf05c508338dc2c4dc4034b89a8ec8aa5d
Destin Lee <dleeinc@gmail.com>
2023-11-13 14:56:24 -0600

    ...

--- app/(protected)/user/page.tsx
+++ app/(protected)/user/page.tsx
@@ -1,3 +1,41 @@
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+import Link from 'next/link'
+
 export default async function UserPage() {
-  return <>user</>
+  const supabase = createServerComponentClient({ cookies })
+
+  const { data: favorites, error } = await supabase.from('favorites').select(`
+    id, ...chapters(title, number) 
+  `)
+
+  // const { data: favorites, error } = await supabase.from('favorites').select(
+  //   `
+  //   id, chapters(id, number, title)
+  // `,
+  // )
+
+  console.log(favorites)
+
+  if (!favorites) {
+    return <h2>No chapters</h2>
+  }
+
+  return (
+    <>
+      <div className='m-auto max-w-4xl'>
+        <h3>favorites</h3>
+        <div>
+          {favorites.map(f => {
+            // return (
+            //   <Link href={`/chapters/${String(f.chapters.number)}`} key={f.id}>
+            //     {f.chapters.title}
+            //   </Link>
+            // )
+            return <pre>{JSON.stringify(f, null, 2)}</pre>
+          })}
+        </div>
+      </div>
+    </>
+  )
 }
