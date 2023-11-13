c753441b109069899e6edf922b69e9042976012a
Destin Lee <dleeinc@gmail.com>
2023-11-13 16:16:56 -0600

    yeahgaa

--- app/chapters/[id]/page.tsx
+++ app/chapters/[id]/page.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, ResolvingMetadata } from 'next'
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
+import FavButton from '@/app/components/FavButton'
 
 type Props = {
   params: { id: string }
@@ -36,7 +37,8 @@ export default async function ChapterPage({
       id,
       title,
       number,
-      sections(id, section_order, lines(id, line_order, text)) 
+      sections(id, section_order, lines(id, line_order, text)),
+      favorites(id) 
     `,
     )
     .eq('number', id)
@@ -53,20 +55,7 @@ export default async function ChapterPage({
           <h2 className='text-2xl font-bold'>{chapter.title}</h2>
         </div>
         <nav className='flex gap-2'>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            fill='none'
-            viewBox='0 0 24 24'
-            strokeWidth={1.5}
-            stroke='currentColor'
-            className='w-6 h-6'
-          >
-            <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              d='M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z'
-            />
-          </svg>
+          <FavButton chapter_id={chapter.id} fav_id={chapter.favorites[0].id} />
           <svg
             xmlns='http://www.w3.org/2000/svg'
             fill='none'
