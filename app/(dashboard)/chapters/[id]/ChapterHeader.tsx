592eb160a23355b08e3741657be1ea20bfec4bb4
Destin Lee <dleeinc@gmail.com>
2023-11-16 10:59:41 -0600

    taging more

--- app/(dashboard)/chapters/[id]/ChapterHeader.tsx
+++ app/(dashboard)/chapters/[id]/ChapterHeader.tsx
@@ -131,6 +131,28 @@ export default function ChapterHeader({
     }
   }
 
+  async function handleDeleteTag(id: string) {
+    const supabase = createClientComponentClient()
+    const { data, error } = await supabase
+      .from('user_tags')
+      .delete()
+      .eq('id', id)
+      .select()
+
+    if (error) {
+      console.error(error)
+      return
+    }
+
+    if (data) {
+      setTags(p => {
+        return p.filter(t => {
+          return t.user_tag_id !== data[0].id
+        })
+      })
+    }
+  }
+
   return (
     <>
       <header className='flex flex-col gap-8 border-b-2 py-4 mb-8 border-dotted'>
@@ -143,13 +165,36 @@ export default function ChapterHeader({
             <div className='flex gap-2 flex-wrap'>
               {tags?.map(t => {
                 return (
-                  <Link
-                    className='p-1 rounded-md bg-orange-600'
-                    href={`/tags/${t.tag_id}`}
-                    key={t.tag_id}
-                  >
-                    {t.tag_text}
-                  </Link>
+                  <div key={t.tag_id} className='flex'>
+                    {tagging && (
+                      <button
+                        className='pr-2 text-red-500'
+                        title={`Delete tag: ${t.tag_text}`}
+                        onClick={() => handleDeleteTag(t.user_tag_id)}
+                      >
+                        <svg
+                          xmlns='http://www.w3.org/2000/svg'
+                          fill='none'
+                          viewBox='0 0 24 24'
+                          strokeWidth={1.5}
+                          stroke='currentColor'
+                          className='w-6 h-6'
+                        >
+                          <path
+                            strokeLinecap='round'
+                            strokeLinejoin='round'
+                            d='M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0'
+                          />
+                        </svg>
+                      </button>
+                    )}
+                    <Link
+                      className='p-1 rounded-md bg-orange-600'
+                      href={`/tags/${t.tag_id}`}
+                    >
+                      {t.tag_text}
+                    </Link>
+                  </div>
                 )
               })}
             </div>
